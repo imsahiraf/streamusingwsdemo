b5cff61400304e7f7c1ee645972ef84e8589dc6e
imsahiraf <zulfekarshaikh0@gmail.com>
2023-04-27 13:34:55 +0530

    With Publisher and Subscriber

--- publisher.js
+++ publisher.js
@@ -18,7 +18,7 @@ const generateRandomNumber = () => {
     const randomNumber = Math.floor(Math.random() * 100000); // generate random number between 0 and 100,000
     console.log('Generated random number:', randomNumber); // add this line to check the value of randomNumber
     redisserver.publish("random", randomNumber.toString());
-    
+
 }
 
 setInterval(generateRandomNumber, 1000); // generate a random number every second
--- subscriber.js
+++ subscriber.js
@@ -13,12 +13,12 @@ let lastIndex = -1;
 console.log("Connecting to the Redis");
 
 subscriber.on("ready", () => {
-  console.log("Connected!");
+    console.log("Connected!");
 });
 subscriber.connect();
 
 subscriber.on("error", (err) => {
-  console.log("Error in the Connection");
+    console.log("Error in the Connection");
 });
 
 const secret = 'my_secret_key';
@@ -33,8 +33,8 @@ mongoose.connect('mongodb://localhost/websocketdemo');
 
 // Define the user schema
 const userSchema = new mongoose.Schema({
-  username: String,
-  password: String
+    username: String,
+    password: String
 });
 
 
@@ -43,114 +43,107 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 app.post('/login', function (req, res) {
-  const { username, password } = req.body;
+    const { username, password } = req.body;
 
-  User.findOne({ username })
-    .then(user => {
-      if (!user) {
-        res.status(401).send('Invalid username or password');
-        return;
-      } else {
-        // First check if the user is already logged in or not
-        if (user.username && loggedInUsers.includes(user.username)) {
-          res.status(402).send('You are already logged in!');
-        } else if (user && user.password === password) {
-          // Generate JWT token with a secret key and set the token as a cookie
-          const token = jwt.sign({ username }, secret);
-          res.status(200).send(token);
-        } else {
-          res.status(401).send('Invalid username or password');
-        }
-      }
-      console.log(user.username)
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).send('Error retrieving user from database');
-    });
+    User.findOne({ username })
+        .then(user => {
+            if (!user) {
+                res.status(401).send('Invalid username or password');
+                return;
+            } else {
+                // First check if the user is already logged in or not
+                if (user.username && loggedInUsers.includes(user.username)) {
+                    res.status(402).send('You are already logged in!');
+                } else if (user && user.password === password) {
+                    // Generate JWT token with a secret key and set the token as a cookie
+                    const token = jwt.sign({ username }, secret);
+                    res.status(200).send(token);
+                } else {
+                    res.status(401).send('Invalid username or password');
+                }
+            }
+            console.log(user.username)
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).send('Error retrieving user from database');
+        });
 });
 
 app.listen(8080, function () {
-  console.log('Server started on port 8080');
+    console.log('Server started on port 8080');
 });
 
 wss.on('connection', function connection(ws, req) {
-  // console.log('Client connected', req);
+    // console.log('Client connected', req);
 
-  const token = req.headers['sec-websocket-protocol'];
+    const token = req.headers['sec-websocket-protocol'];
 
-  if (!token) {
-    console.log(token)
-    ws.close();
-    return;
-  }
-
-  jwt.verify(token, secret, function (err, decoded) {
-    if (err) {
-      console.log(err);
-      ws.close();
-      return;
+    if (!token) {
+        console.log(token)
+        ws.close();
+        return;
     }
 
-    console.log('Token verified:', decoded);
+    jwt.verify(token, secret, function (err, decoded) {
+        if (err) {
+            console.log(err);
+            ws.close();
+            return;
+        }
 
-    // Add the user to the list of logged in users
-    const user = decoded.username;
-    if (user) {
-      loggedInUsers.push(user);
-    } else {
-      console.log(`User ${decoded.username} not found in database`);
-    }
+        console.log('Token verified:', decoded);
 
-    console.log(loggedInUsers)
+        // Add the user to the list of logged in users
+        const user = decoded.username;
+        if (user) {
+            loggedInUsers.push(user);
+        } else {
+            console.log(`User ${decoded.username} not found in database`);
+        }
 
-    // Send any new numbers from Redis to the client every second
-    // const redisInterval = setInterval(() => {
-    //   showNewNumbersFromRedis();
-    // }, 1000);
-    
-    subscriber.subscribe('random', (message) => {
-      ws.send(message)
-      console.log("Here the random generated number for a second is" + message); // 'message'
-      });
+        console.log(loggedInUsers)
 
-    ws.on('close', function close() {
-      console.log('Client disconnected');
-      // Remove the user from the list of logged in users
-      const index = loggedInUsers.findIndex(u => u === decoded.username);
-      if (index !== -1) {
-        loggedInUsers.splice(index, 1);
-      }
-      console.log(loggedInUsers)
-      clearInterval(redisInterval);
+        subscriber.subscribe('random', (message) => {
+            ws.send(message)
+        });
+
+        ws.on('close', function close() {
+            console.log('Client disconnected');
+            // Remove the user from the list of logged in users
+            const index = loggedInUsers.findIndex(u => u === decoded.username);
+            if (index !== -1) {
+                loggedInUsers.splice(index, 1);
+            }
+            console.log(loggedInUsers)
+        });
     });
-  });
 });
 
 
 // API endpoint to add a user
 app.post('/adduser', function (req, res) {
-  const { username, password } = req.body;
-  console.log(req.body)
-  User.findOne({ username })
-    .then(existingUser => {
-      if (existingUser) {
-        res.status(409).send('User already exists');
-      } else {
-        const user = new User({ username, password });
+    const { username, password } = req.body;
+    console.log(req.body)
+    User.findOne({ username })
+        .then(existingUser => {
+            if (existingUser) {
+                res.status(409).send('User already exists');
+            } else {
+                const user = new User({ username, password });
 
-        user.save()
-          .then(() => {
-            res.status(200).send('User added successfully');
-          })
-          .catch(err => {
+                user.save()
+                    .then(() => {
+                        res.status(200).send('User added successfully');
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        res.status(500).send('Error saving user to database');
+                    });
+            }
+        })
+        .catch(err => {
             console.error(err);
-            res.status(500).send('Error saving user to database');
-          });
-      }
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).send('Error checking for existing user');
-    });
+            res.status(500).send('Error checking for existing user');
+        });
 });
