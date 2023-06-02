91c3e2978c2e6063b6c2e75d22e97338a0b18631
imsahiraf <zulfekarshaikh0@gmail.com>
2023-06-02 18:21:06 +0530

    Mongo Shard Coded

---
+++ mongoshard/mongo.js
@@ -0,0 +1,59 @@
+const { MongoClient } = require('mongodb');
+
+const uri = 'mongodb://<username>:<password>@<mongos-hostname>:<mongos-port>/<database-name>';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect((err) => {
+    if (err) {
+        console.log('Error connecting to the database:', err);
+        return;
+    }
+
+    const db = client.db('<database-name>');
+    const collection = db.collection('<collection-name>');
+
+    // Perform CRUD operations
+    // Example operations:
+    // 1. Create: Insert a document
+    const documentToInsert = { name: 'John', age: 30 };
+    collection.insertOne(documentToInsert, (err, result) => {
+        if (err) {
+            console.log('Error inserting document:', err);
+            return;
+        }
+        console.log('Inserted document:', result.ops);
+
+        // 2. Read: Find documents
+        collection.find({ name: 'John' }).toArray((err, docs) => {
+            if (err) {
+                console.log('Error querying collection:', err);
+                return;
+            }
+            console.log('Documents:', docs);
+
+            // 3. Update: Update a document
+            const filter = { name: 'John' };
+            const update = { $set: { age: 31 } };
+            collection.updateOne(filter, update, (err, result) => {
+                if (err) {
+                    console.log('Error updating document:', err);
+                    return;
+                }
+                console.log('Updated document:', result.modifiedCount);
+
+                // 4. Delete: Delete a document
+                const deleteFilter = { name: 'John' };
+                collection.deleteOne(deleteFilter, (err, result) => {
+                    if (err) {
+                        console.log('Error deleting document:', err);
+                        return;
+                    }
+                    console.log('Deleted document:', result.deletedCount);
+
+                    // Close the connection
+                    client.close();
+                });
+            });
+        });
+    });
+});
